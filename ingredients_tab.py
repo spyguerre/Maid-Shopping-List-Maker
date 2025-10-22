4f1908d2c6c927f46254f55cb605bf260712a7e3
Alexandre P <alexandre.padellini@telecomnancy.eu>
2025-10-22 11:22:33 +0200

    Added drink recipes & their ingredients

--- ingredients_tab.py
+++ ingredients_tab.py
@@ -15,17 +15,19 @@ def generate_ingredients_tab():
     cells1[0] = ["Recipe", "Pieces", "Quantity", "Unit", "Ingredient", "", "Mult. Qty", "Unit", "Ingredient"]
 
     recipe_index = 0
-    for recipe, url in RECIPES.items():
+    for recipe, data in RECIPES.items():
+        url, ing_start_cell, is_single_piece_recipe = data["url"], data["ing_cell"], data["is_single-piece_recipe"]
+
         cells1[recipe_index * 22 + 1][0] = f'=HYPERLINK("{url}", "{recipe}")'
-        cells1[recipe_index * 22 + 1][1] = f'=IMPORTRANGE("{url}", "RECETTE!L2")'
+        cells1[recipe_index * 22 + 1][1] = f'=IMPORTRANGE("{url}", "RECETTE!L2")' if not is_single_piece_recipe else "/"
 
         cells1[recipe_index * 22 + 2][0] = "Target pieces:"
         cells1[recipe_index * 22 + 2][1] = "0"
 
-        for i in range(3, 23):  # Assuming max 20 ingredients per recipe
+        for i in range(ing_start_cell, 23):  # Assuming max 20 ingredients per recipe
             qty_formula = f'=IMPORTRANGE("{url}", "RECETTE!J{i}")'
             cells1[recipe_index * 22 + i - 1][2] = qty_formula
-            mult_qty_formula = f'=IF({get_excel_cell(recipe_index * 22 + i - 1, 4)}=0; \"\"; {get_excel_cell(recipe_index * 22 + i - 1, 2)}*CEILING({get_excel_cell(recipe_index * 22 + 2, 1, True, True)} / {get_excel_cell(recipe_index * 22 + 1, 1, True, True)}))'
+            mult_qty_formula = f'=IF({get_excel_cell(recipe_index * 22 + i - 1, 4)}=0; \"\"; {get_excel_cell(recipe_index * 22 + i - 1, 2)}*CEILING({get_excel_cell(recipe_index * 22 + 2, 1, True, True)} / {get_excel_cell(recipe_index * 22 + 1, 1, True, True) if not is_single_piece_recipe else 1}))'
             cells1[recipe_index * 22 + i - 1][6] = mult_qty_formula
 
             unit_formula = f'=IMPORTRANGE("{url}", "RECETTE!K{i}")'
@@ -35,8 +37,6 @@ def generate_ingredients_tab():
             ing_formula = f'=IMPORTRANGE("{url}", "RECETTE!L{i}")'
             cells1[recipe_index * 22 + i - 1][4] = ing_formula
             cells1[recipe_index * 22 + i - 1][8] = ing_formula
-
-
 
         recipe_index += 1
 
